71cfc947dae18c9f129f78edd17b99d04c0472d4
mariah622 <mjustine622@gmail.com>
2022-02-15 09:57:34 -0500

    commit tree da1e495ee45d1f7b1745f1a2dd41dac828b4f78d

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,32 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: [],
+}, action){
+
+    switch(action.type) {
+
+        case "ADD_FRIEND":
+          return (
+            {...state,
+                friends: [
+                  ...state.friends,
+                  action.friend
+                ]
+            }
+        )
+    
+        case "REMOVE_FRIEND":
+          const removal = state.friends.findIndex(friend => friend.id === action.id);
+          return (
+            {...state,
+                friends: [
+                  ...state.friends.slice(0, removal),
+                  ...state.friends.slice(removal + 1)
+                ]
+            }
+          )
+    
+        default:
+          return state;
+    
+      }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,17 @@
-export function managePresents(state, action){
+
+
+export function managePresents(state = {numberOfPresents: 0}, action){
+    state ={
+        numberOfPresents: 0
+
+    }
+
+    switch(action.type){
+        case 'INCREASE':
+            return {numberOfPresents: state.numberOfPresents + 1};
+            default:
+                return state
+
+    }
+
 }
